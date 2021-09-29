adde116498aea7bf9a58505d20cadc4ae1ecdafe
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-09-29 23:50:22 +0700

    Add google ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,38 @@
-
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import LayoutComponent from '../components/Layout'
-import '../styles/globals.css'
-
-
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import LayoutComponent from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(false);
   const handleDark = () => {
-    setIsDark(!isDark)
-    localStorage.setItem('dark', !isDark)
-  }
+    setIsDark(!isDark);
+    localStorage.setItem("dark", !isDark);
+  };
   useEffect(() => {
-    const check = JSON.parse(localStorage.getItem('dark'))
-    setIsDark(check)
-  }, [])
+    const check = JSON.parse(localStorage.getItem("dark"));
+    setIsDark(check);
+  }, []);
 
   return (
     <>
-
-      <div className={isDark ? 'app dark  ' : 'app '}>
+      <div className={isDark ? "app dark  " : "app "}>
         <LayoutComponent>
           <Head>
-            <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet' />
+            <link
+              href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
+              rel="stylesheet"
+            />
 
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=UA-208281120-1"
+            />
+            <script>
+              window.dataLayer = window.dataLayer || []; function gtag()
+              {dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'UA-208281120-1');
+            </script>
           </Head>
           <Component {...pageProps} />
           <div className="checkbox">
@@ -37,7 +45,7 @@ function MyApp({ Component, pageProps }) {
         </LayoutComponent>
       </div>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
