1c25c81e3249af6ebf330a03a74ee534cbfab59c
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-09-30 00:04:04 +0700

    Add gg ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import LayoutComponent from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   const [isDark, setIsDark] = useState(false);
   const handleDark = () => {
     setIsDark(!isDark);
@@ -14,6 +16,16 @@ function MyApp({ Component, pageProps }) {
     setIsDark(check);
   }, []);
 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <div className={isDark ? "app dark  " : "app "}>
@@ -23,16 +35,23 @@ function MyApp({ Component, pageProps }) {
               href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
               rel="stylesheet"
             />
-
             <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=UA-208281120-1"
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=UA-208281120-1`}
             />
-            <script>
-              window.dataLayer = window.dataLayer || []; function gtag()
-              {dataLayer.push(arguments)}
-              gtag('js', new Date()); gtag('config', 'UA-208281120-1');
-            </script>
+            <script
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'UA-208281120-1', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
           </Head>
           <Component {...pageProps} />
           <div className="checkbox">
